f7296de21bc86c6d104cfa5a74827bace54183e6
emanchal <168318813+emanchal@users.noreply.github.com>
2024-04-28 08:16:41 +0530

    Create Priority counter.sv

---
+++ Priority counter.sv
@@ -0,0 +1,17 @@
+//write a 4 bit priority counter 
+//truth table
+//   inputs       output
+//  p3 p2 p1 p0   y
+//  0  0  0  0    0
+//  1  0  0  0    p3
+//  0  1  0  0    p2
+//  0  0  1  0    p1
+//  0  0  0  1    p0
+
+module priority_counter;
+  inputs
+  outputs
+
+  design code
+
+endmodule
