fec475870d960fcde88a5d9dcb391ebe8e986597
emanchal <168318813+emanchal@users.noreply.github.com>
2024-12-01 12:24:13 +0530

    Create 2_verilog_template.v

---
+++ 2_verilog_template.v
@@ -0,0 +1,16 @@
+/*
+verilog sections template
+*/
+
+module [design_name] ([port_list]);
+
+  [list_of_input_ports];
+  [list_of_output_posts];
+
+  [declaration_of_other_signlas];
+
+  [other_module_instantiations_if_required];
+
+  [behavioural_code_for_this_moduele];
+
+endmodule
